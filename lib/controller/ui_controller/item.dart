5e913016b386c079650c81a2321059f16df4cd5a
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-03 23:25:43 -0800

    first commit

--- lib/controller/ui_controller/item.dart
+++ lib/controller/ui_controller/item.dart
@@ -1,11 +1,22 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:provider_dio_practice/model/item.dart';
 
-class ItemProvider with ChangeNotifier{
-
+class ItemProvider with ChangeNotifier {
   List<Item> _items = [];
   bool _isLoading = false;
 
   List<Item> get items => _items;
+  bool get isLoading => _isLoading;
 
+  Future<void> fetchItems() async {
+    _isLoading = true;
+    notifyListeners();
+    try {} catch (e) {
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
 }
