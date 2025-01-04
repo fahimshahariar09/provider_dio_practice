8f215abe0db450aa7e48814f9023af61edf9e339
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-03 23:34:11 -0800

    first commit

--- lib/view/screen/item.dart
+++ lib/view/screen/item.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_dio_practice/controller/ui_controller/item.dart';
 
 class Item extends StatelessWidget {
   const Item({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    ItemProvider itemProvider = Provider.of<ItemProvider>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Items"),
+      ),
+    );
   }
 }
