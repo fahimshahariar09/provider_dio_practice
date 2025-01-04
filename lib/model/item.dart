ff45e404b652e17a11a914004406b82a800d7a85
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-03 22:57:01 -0800

    first commit

--- lib/model/item.dart
+++ lib/model/item.dart
@@ -2,4 +2,12 @@
 class Item{
   final int id ;
   final String name;
+  
+  Item({required this.id, required this.name});
+  
+  factory Item.fromJson(Map<String ,dynamic> json){
+    return Item(id: id, name: name)
+  }
+  
+  
 }
