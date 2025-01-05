a83a6a5498cb96578d7c05178dee680c89a4ed90
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-04 23:53:04 -0800

    first commit

--- lib/view/screen/home.dart
+++ lib/view/screen/home.dart
@@ -1,8 +1,14 @@
+import 'package:flutter/material.dart';
+
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("data"),
+      ),
+    );
   }
 }
