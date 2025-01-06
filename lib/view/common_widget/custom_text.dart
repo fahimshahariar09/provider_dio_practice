b98d5f299a1059855d488d31dc206d6c887ca584
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-05 23:40:03 -0800

    first commit

--- lib/view/common_widget/custom_text.dart
+++ lib/view/common_widget/custom_text.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 
 class CustomText extends StatelessWidget {
-  const CustomText({super.key, required this.text});
+  const CustomText({super.key, required this.text, this.fontColor, this.fontSize});
 
   final String text;
+  final Color? fontColor;
+  final double? fontSize;
 
   @override
   Widget build(BuildContext context) {
     return Text(
+      textDirection: TextDecoration.lineThrough,
       text,
       style: TextStyle(
-          color: Colors.white,
-          fontSize: 15,
+          color: fontColor,
+          fontSize: fontSize ?? 15,
           fontWeight: FontWeight.normal,
           fontFamily: "RobotoSerif"),
       maxLines: 1,
