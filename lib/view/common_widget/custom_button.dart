fa8279e46a3850ac376abe5ac31874a2442f6962
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-11 21:24:30 -0800

    first commit

--- lib/view/common_widget/custom_button.dart
+++ lib/view/common_widget/custom_button.dart
@@ -2,16 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:provider_dio_practice/view/common_widget/custom_text.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.buttonName});
+  const CustomButton(
+      {super.key, required this.buttonName, required this.onTap});
 
   final String buttonName;
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: SizedBox(
-        height: 45,
-        child: CustomText(text: buttonName),
+    return InkWell(
+      onTap: onTap,
+      child: Card(
+        color: Colors.orange,
+        child: SizedBox(
+          height: 45,
+          child: CustomText(text: buttonName),
+        ),
       ),
     );
   }
