42d1a1a9652505f6305f07da280fc6202f9271e0
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-11 21:23:14 -0800

    first commit

--- lib/view/common_widget/custom_button.dart
+++ lib/view/common_widget/custom_button.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider_dio_practice/view/common_widget/custom_text.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
+  const CustomButton({super.key, required this.buttonName});
+
+  final String buttonName;
 
   @override
   Widget build(BuildContext context) {
-    return Card();
+    return Card(
+      child: SizedBox(
+        height: 45,
+        child: CustomText(text: buttonName),
+      ),
+    );
   }
 }
