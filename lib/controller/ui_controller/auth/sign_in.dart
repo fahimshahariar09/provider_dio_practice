ca8d6adcbc263da218c92468cfa32589a24597d6
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-17 22:43:30 -0800

    first commit

--- lib/controller/ui_controller/auth/sign_in.dart
+++ lib/controller/ui_controller/auth/sign_in.dart
@@ -1,7 +1,10 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class SignInController extends GetxController{
 
   RxBool isLoading = true.obs;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passController = TextEditingController();
 }
