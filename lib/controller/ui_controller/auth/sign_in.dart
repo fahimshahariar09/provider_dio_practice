f3c4246e5c771fdf7e2adf5f6ef4f5e45929e35d
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-17 22:48:25 -0800

    first commit

--- lib/controller/ui_controller/auth/sign_in.dart
+++ lib/controller/ui_controller/auth/sign_in.dart
@@ -1,10 +1,16 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:provider_dio_practice/controller/api_controller/auth/sign_in.dart';
 
-class SignInController extends GetxController{
-
+class SignInController extends GetxController {
   RxBool isLoading = true.obs;
   TextEditingController emailController = TextEditingController();
   TextEditingController passController = TextEditingController();
+
+  signinFun() async {
+    isLoading.value = true;
+    SignInService.signinService(
+        email: emailController.text, pass: passController.text);
+    isLoading.value = false;
+  }
 }
