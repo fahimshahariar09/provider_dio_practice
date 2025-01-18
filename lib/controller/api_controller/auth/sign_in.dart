6e780c7e0227ac1bb3cf86c588bc9f876a3118c7
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-17 22:45:44 -0800

    first commit

--- lib/controller/api_controller/auth/sign_in.dart
+++ lib/controller/api_controller/auth/sign_in.dart
@@ -1,7 +1,10 @@
+import 'dart:developer';
 
-
-class SignInService{
-  static Future signinService(){
-    try{}
+class SignInService {
+  static Future signinService() async {
+    try {} catch (e) {
+      log("error $e");
+    }
+    return false;
   }
 }
