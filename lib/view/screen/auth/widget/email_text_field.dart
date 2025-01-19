123e0407bd49921a0a607669bbd8e672e635861e
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-18 21:50:32 -0800

    first commit

--- lib/view/screen/auth/widget/email_text_field.dart
+++ lib/view/screen/auth/widget/email_text_field.dart
@@ -1,8 +1,17 @@
+import 'package:flutter/material.dart';
+
 class EmailTextField extends StatelessWidget {
   const EmailTextField({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return TextFormField(
+      decoration: InputDecoration(
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: Colors.red,width: 1),
+        )
+      ),
+    );
   }
 }
