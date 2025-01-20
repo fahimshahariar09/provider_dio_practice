f6f702f403fe1e54d8db64b528d7d629f4edb1de
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-19 21:51:22 -0800

    first commit

---
+++ lib/view/screen/auth/widget/password_text_field.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class EmailTextField extends StatelessWidget {
+  const EmailTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: Colors.red, width: 1),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: Colors.red, width: 1),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: Colors.red, width: 1),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: Colors.red, width: 1),
+          ),
+          hintText: "Email",
+          prefixIcon: Icon(Icons.email)
+      ),
+      validator: (value){
+        if(value == "" || value == null){
+          return "E,ail can't be empty";
+        }else if(!(value.toString().contains('.') || value.contains("@"))){
+          return "Please your valid mail";
+        }
+        return null;
+      },
+    );
+  }
+}
