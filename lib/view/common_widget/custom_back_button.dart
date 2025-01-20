0df07df2e368c379313af69c38598aff5f44fa39
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-19 21:55:12 -0800

    first commit

--- lib/view/common_widget/custom_back_button.dart
+++ lib/view/common_widget/custom_back_button.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/material.dart';
+
 class CustomBackButton extends StatelessWidget {
   const CustomBackButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Icon(Icons.arrow_back, size: 25);
   }
 }
