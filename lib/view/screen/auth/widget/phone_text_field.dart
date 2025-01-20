8f83541af70b3f1862302bf15e0a087726633798
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-19 21:56:35 -0800

    first commit

---
+++ lib/view/screen/auth/widget/phone_text_field.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PhoneTextField extends StatelessWidget {
+  const PhoneTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField();
+  }
+}
