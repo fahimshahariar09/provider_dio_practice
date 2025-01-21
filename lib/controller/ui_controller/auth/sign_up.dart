d9d75612d8b30b5bf4d796c178a9d284babe40d6
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-20 23:30:45 -0800

    first commit

--- lib/controller/ui_controller/auth/sign_up.dart
+++ lib/controller/ui_controller/auth/sign_up.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:provider_dio_practice/controller/api_controller/auth/sign_up.dart';
 
 class SignUpController extends GetxController {
   final formKey = GlobalKey<FormState>();
   RxBool isLoading = true.obs;
   TextEditingController emailController = TextEditingController();
   TextEditingController passController = TextEditingController();
+
+  signupFun() async {
+    isLoading.value = true;
+    SignUpService.signupService(
+        email: emailController.text, pass: passController.text);
+    isLoading.value = false;
+  }
+
 }
