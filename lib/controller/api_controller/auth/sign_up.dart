4074220b1be8dd861bb6ef01095b69159fd5b339
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-20 23:29:19 -0800

    first commit

--- lib/controller/api_controller/auth/sign_up.dart
+++ lib/controller/api_controller/auth/sign_up.dart
@@ -1,3 +1,14 @@
-class SignUpService{
+import 'dart:developer';
 
+class SignUpService {
+  static Future signinService(
+      {required String email, required String pass}) async {
+    try {
+      await Future.delayed(Duration(seconds: 2));
+      return true;
+    } catch (e) {
+      log("error $e");
+    }
+    return false;
+  }
 }
