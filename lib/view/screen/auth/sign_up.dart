49f378ff4baa9cf13c79a74cc8803b77f823fc20
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-21 23:12:48 -0800

    first commit

--- lib/view/screen/auth/sign_up.dart
+++ lib/view/screen/auth/sign_up.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider_dio_practice/controller/ui_controller/auth/sign_up.dart';
 import 'package:provider_dio_practice/view/screen/auth/widget/email_text_field.dart';
+import 'package:provider_dio_practice/view/screen/auth/widget/password_text_field.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({super.key});
@@ -23,12 +24,12 @@ class SignUp extends StatelessWidget {
               EmailTextField(emailController: signUpController.emailController),
               SizedBox(height: 10),
               Text("phone"),
-              TextField(),
+              PasswordTextField(
+                  passController: signUpController.passController),
               SizedBox(height: 10),
               Text("password"),
               TextField(),
               SizedBox(height: 10),
-
             ],
           ),
         ),
