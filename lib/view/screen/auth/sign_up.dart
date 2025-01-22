c2dda7e501fd31d603fa1990c47e342d8ab344e3
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-21 23:12:05 -0800

    first commit

--- lib/view/screen/auth/sign_up.dart
+++ lib/view/screen/auth/sign_up.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:provider_dio_practice/controller/ui_controller/auth/sign_up.dart';
+import 'package:provider_dio_practice/view/screen/auth/widget/email_text_field.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    SignUpController signUpController = Get.put(SignUpController());
     return Scaffold(
       appBar: AppBar(
         title: Text("Sign Up"),
@@ -16,7 +20,7 @@ class SignUp extends StatelessWidget {
           child: Column(
             children: [
               Text("email"),
-              TextField(),
+              EmailTextField(emailController: signUpController.emailController),
               SizedBox(height: 10),
               Text("phone"),
               TextField(),
