5d2e801f6838d5d9702b40aec6608b904d7219a5
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-21 23:14:11 -0800

    first commit

--- lib/view/screen/auth/widget/phone_text_field.dart
+++ lib/view/screen/auth/widget/phone_text_field.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
 
 class PhoneTextField extends StatelessWidget {
-  const PhoneTextField({super.key});
+  const PhoneTextField({super.key, required this.phoneController});
+
+  final TextEditingController phoneController;
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField();
+    return TextFormField(
+      controller: phoneController,
+      decoration: InputDecoration(
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: Colors.red, width: 1),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: Colors.red, width: 1),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: Colors.red, width: 1),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: Colors.red, width: 1),
+        ),
+        hintText: "Password",
+        prefixIcon: Icon(Icons.lock),
+        suffixIcon: Icon(Icons.remove_red_eye_outlined),
+      ),
+      validator: (value) {
+        if (value == "" || value == null) {
+          return "Email can't be empty";
+        } else if (!(value.toString().contains('.') || value.contains("@"))) {
+          return "Please your valid mail";
+        }
+        return null;
+      },
+    );
   }
 }
