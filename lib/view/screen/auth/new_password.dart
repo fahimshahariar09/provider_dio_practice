55e73436c2e05df84e308f1075cc07ccea7dc339
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-26 21:29:08 -0800

    first commit

--- lib/view/screen/auth/new_password.dart
+++ lib/view/screen/auth/new_password.dart
@@ -1,8 +1,14 @@
+import 'package:flutter/material.dart';
+
 class NewPassword extends StatelessWidget {
   const NewPassword({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New Password"),
+      ),
+    );
   }
 }
