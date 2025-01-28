05fdd20cbec0ef227a824b860dcf509e4862c314
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-01-27 18:33:09 -0800

    first commit

--- lib/view/screen/auth/new_password.dart
+++ lib/view/screen/auth/new_password.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider_dio_practice/controller/ui_controller/auth/new_password.dart';
+import 'package:provider_dio_practice/view/common_widget/custom_button.dart';
 import 'package:provider_dio_practice/view/screen/auth/widget/password_text_field.dart';
 
 class NewPassword extends StatelessWidget {
@@ -19,7 +20,10 @@ class NewPassword extends StatelessWidget {
           child: Column(
             children: [
              PasswordTextField(passController: newPasswordController.passwordController),
-             PasswordTextField(passController: newPasswordController.passwordController),
+             SizedBox(height: 10),
+             PasswordTextField(passController: newPasswordController.newpasswordController),
+              SizedBox(height: 20),
+              CustomButton(buttonName: "change password", onTap: (){})
             ],
           ),
         ),
