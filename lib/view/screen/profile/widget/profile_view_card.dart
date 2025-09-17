659615b776129f4a2ecfc8477bb69b56437b4bf7
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2025-09-17 22:00:02 +0600

    ProfileViewCard

---
+++ lib/view/screen/profile/widget/profile_view_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProfileViewCard extends StatelessWidget {
+  const ProfileViewCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row();
+  }
+}
