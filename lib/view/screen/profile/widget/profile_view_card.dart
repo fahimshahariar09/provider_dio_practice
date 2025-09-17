ddb1c053c8447ab76eab42185b92cdbc659f10d7
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2025-09-17 22:03:29 +0600

    ProfileViewCard

--- lib/view/screen/profile/widget/profile_view_card.dart
+++ lib/view/screen/profile/widget/profile_view_card.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 class ProfileViewCard extends StatelessWidget {
-  const ProfileViewCard({super.key});
+  const ProfileViewCard({super.key, required this.title, required this.icon});
 
+  final String title;
+  final Widget icon;
   @override
   Widget build(BuildContext context) {
-    return Row();
+    return Row(
+      children: [
+        icon,
+        SizedBox(width: 10),
+        Text(
+          title,
+          style: TextStyle(
+              color: Colors.blue, fontWeight: FontWeight.w500, fontSize: 15),
+        )
+      ],
+    );
   }
 }
