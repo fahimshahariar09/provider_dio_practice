7a404c35f7882ef7cc0f05b418522e6a8efca386
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2025-09-21 22:17:22 +0600

    Home

--- lib/view/screen/home.dart
+++ lib/view/screen/home.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("data"),
-      ),
       body: Column(
         children: [
-          Text("data")
+          FutureBuilder(
+              future: future,
+              builder: (context, snackshot) {
+                return Card();
+              })
         ],
       ),
     );
