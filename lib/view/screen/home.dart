98d1701ca8167ec7cb1fefa44622bb37d4e5fec7
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2025-09-22 20:39:44 +0600

    Home

--- lib/view/screen/home.dart
+++ lib/view/screen/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider_dio_practice/view/common_widget/custom_text.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -19,50 +20,59 @@ class _HomeState extends State<Home> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          FutureBuilder<String>(
-            future: fetchData(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              } else if (snapshot.hasError) {
-                return Center(
-                  child: Text(
-                    "Error: ${snapshot.error}",
-                    style: const TextStyle(color: Colors.red, fontSize: 16),
-                  ),
-                );
-              } else if (snapshot.hasData) {
-                return Card(
-                  color: Colors.blue,
-                  elevation: 4,
-                  margin: const EdgeInsets.all(16),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
+          Expanded(
+            child: FutureBuilder<String>(
+              future: fetchData(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(
                     child: Text(
-                      snapshot.data!,
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
+                      "Error: ${snapshot.error}",
+                      style: const TextStyle(color: Colors.red, fontSize: 16),
+                    ),
+                  );
+                } else if (snapshot.hasData) {
+                  return Card(
+                    color: Colors.blue,
+                    elevation: 4,
+                    margin: const EdgeInsets.all(16),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        snapshot.data!,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
+                    ),
+                  );
+                }
+                return const Card(
+                  color: Colors.grey,
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      "No data available",
+                      style: TextStyle(color: Colors.white),
                     ),
                   ),
                 );
-              }
-              return const Card(
-                color: Colors.grey,
-                child: Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text(
-                    "No data available",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              );
-            },
+              },
+            ),
           ),
+          ListView.builder(itemBuilder: (context, index) {
+            return Column(
+              children: [
+                CustomText(text: "text"),
+              ],
+            );
+          })
         ],
       ),
     );
